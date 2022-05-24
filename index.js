d484cf46d3560ad4358e1a0447c575076740079f
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-05-24 23:18:37 +0700

    Add js

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-table {
-    border: 5px solid black;
+let output = 0
+let mem = 0
+let operation = 0
+let outputEl = document.getElementById("display")
+
+function num(x) {
+    outputEl.innerText = outputEl.innerText*10 + x
+}
+
+function op(x) {
+    mem = parseFloat(outputEl.innerText)
+    outputEl.innerText = 0
+    operation = x
+}
+
+function enter() {
+    switch(operation) {
+        case 0: //reset
+            outputEl.innerText = 0
+            break;
+        case 1: //+
+            outputEl.innerText = mem + parseFloat(outputEl.innerText)
+            break;
+        case 2: //-
+            outputEl.innerText = mem - parseFloat(outputEl.innerText)
+            break;
+        case 3: //*
+            outputEl.innerText = mem * parseFloat(outputEl.innerText)
+            break;
+        case 4: //divide
+            outputEl.innerText = mem / parseFloat(outputEl.innerText)
+            break;
+    }
+    operation = 0
 }
